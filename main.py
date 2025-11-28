cfea20010b79cf40713d8057110ecd231ebd546c
EllisSK <ellissinclairkent@gmail.com>
2025-11-28 21:05:46 +0000

    Implemented final optimiser and main sections, ready to debug

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
+import sys
+
 from pathlib import Path
 from pymoo.optimize import minimize
 from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.termination import get_termination
+from pymoo.parallelization.starmap import StarmapParallelization
+from multiprocessing import cpu_count, Manager
+from multiprocessing.pool import ThreadPool
 
 from shetran_interaction import *
 from results_analysis import *
 from optimiser import ShetranProblem
 
 def main():
+    
     config = load_shetran_params(Path("temp/config.json"))
 
-    algorithm = NSGA2(pop_size=20, n_offsprings=10, eliminate_duplicates=True)
+    settings = {
+        "catchment_name" : sys.argv[1],
+        "base_project_directory" : sys.argv[2],
+        "observed_data" : sys.argv[3],
+        "shetran_path" : sys.argv[4],
+        "preprocessor_path" : sys.argv[5]
 
-    problem = ShetranProblem()
+    }
 
-    res = minimize(problem, algorithm, verbose=True)
+    with Manager() as manager:
+        shared_lock = manager.Lock()
 
-    print("Optimisation Complete.")
-    print(f"Time taken: {res.exec_time} seconds")
+        n_threads = max(1, cpu_count() - 4) 
+        pool = ThreadPool(n_threads)
+        runner = StarmapParallelization(pool.starmap)
+
+        algorithm = NSGA2(pop_size=50, n_offsprings=10, eliminate_duplicates=True)
+
+        problem = ShetranProblem(config, settings, shared_lock, elementwise_runner=runner)
+
+        res = minimize(problem, algorithm, termination=get_termination("n_gen", 40), verbose=True)
+
+        pool.close()
+
+        print("Optimisation Complete.")
+        print(f"Time taken: {res.exec_time} seconds")
 
 if __name__ == "__main__":
     main()
--- optimiser.py
+++ optimiser.py
@@ -1,3 +1,9 @@
+import uuid
+import os
+import shutil
+import multiprocessing
+import time
+
 import numpy as np
 import pandas as pd
 
@@ -7,10 +13,16 @@ from shetran_interaction import *
 from results_analysis import *
 
 class ShetranProblem(ElementwiseProblem):
-    def __init__(self, simulation_data: Path, observed_data: Path, config: dict, settings: dict):
+    def __init__(self, config: dict, settings: dict, lock, **kwargs):
         self.settings = settings
-        self.observed_path = observed_data
-        self.data_path = simulation_data
+
+        self.base_dir = Path(f"{self.settings["base_project_directory"]}")
+        self.master_xml = self.base_dir / f"{self.settings["catchment_name"]}_Library_File.xml"
+        self.preprocessor = Path(self.settings["preprocessor_path"])
+        self.shetran = Path(self.settings["shetran_path"])
+        self.observed = self.base_dir / f"{self.settings["observed_data"]}"
+        self.log = self.base_dir / "log.csv"
+        self.lock = lock
         
         params_to_optimise = []
         
@@ -29,6 +41,14 @@ class ShetranProblem(ElementwiseProblem):
 
         self.pto = params_to_optimise
 
+        param_names = [p["name"] for p in self.pto]
+        objective_fn_names = ["1-KGE","1-LogKGE","RMSE"]
+        header = ["Timestamp", "Run_ID"] + param_names + objective_fn_names
+
+        with open(self.log, "w", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+
         xl = np.array([p["bounds"][0] for p in self.pto])
         xu = np.array([p["bounds"][1] for p in self.pto])
 
@@ -37,4 +57,59 @@ class ShetranProblem(ElementwiseProblem):
             n_obj=3, 
             n_constr=0, 
             xl=xl, 
-            xu=xu)
+            xu=xu,
+            **kwargs
+            )
+        
+    def _evaluate(self, x, out, *args, **kwargs):
+        run_id = uuid.uuid4().hex[:8]
+
+        run_dir = self.base_dir / "runs" / f"run_{run_id}"
+
+        run_xml = run_dir / f"{self.settings["catchment_name"]}_Library_File.xml"
+        rundata = run_dir / f"rundata_{self.settings["catchment_name"]}.txt"
+        run_output = run_dir / f""
+
+        objectives = [1e10, 1e10, 1e10]
+
+        try:
+            os.makedirs(run_dir, exist_ok=True)
+
+            shutil.copy(self.master_xml, run_xml)
+
+            update_dict = {}
+            for i, param_def in enumerate(self.pto):
+                val = x[i]
+                if param_def["table"] not in update_dict:
+                    update_dict[param_def["table"]] = {}
+                if param_def["row_id"] not in update_dict[param_def["table"]]:
+                    update_dict[param_def["table"]][param_def["row_id"]] = {}
+                update_dict[param_def["table"]][param_def["row_id"]][param_def["col_name"]] = val
+
+            modify_xml_file(run_xml, update_dict)
+
+            run_preprocessor(self.preprocessor, run_xml)
+
+            run_shetran(self.shetran, rundata)
+
+            if run_output.exists():
+                objectives = calculate_objective_function_metrics(self.observed, run_output)
+                out["F"] = list(objectives)
+            else:
+                out["F"] = objectives
+        except:
+            out["F"] = objectives
+        finally:
+            try:
+               timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+               log_row = [timestamp, run_id] + list(x) + list(objectives)
+               with self.lock:
+                    with open(self.log, "a", newline="") as f:
+                        writer = csv.writer(f)
+                        writer.writerow(log_row)     
+
+            except Exception as log_err:
+                print(f"Logging failed for {run_id}: {log_err}")
+           
+            if os.path.exists(run_dir):
+                shutil.rmtree(run_dir, ignore_errors=True)
